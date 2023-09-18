652648f527bff14adc5adac5609e68c839645097
mariamnabilibrahim <eng.mariamnabil93@gmail.com>
2023-09-17 23:23:36 -0700

    simple shell

---
+++ parser_fun.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "shell.h"
+
+/**
+ * is_cmd - function to determine the file is executable command
+ * @info: input of info structure
+ * @path: input of path of file
+ * Retuen: returns 1 if success, returns 0 if error
+*/
+
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - function to duplicate characters
+ * @pathstr: input string
+ * @start: input starting index
+ * @stop: input stopping index
+ * Retuen: returns the pointer of new buffer
+*/
+
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int x = 0, y = 0;
+
+	for (y = 0, x = start; x < stop; x++)
+		if (pathstr[x] != ':')
+			buf[y++] = pathstr[x];
+	buf[y] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - function to find path
+ * @info: input of info struct
+ * @pathstr: input string
+ * @cmd: find command
+ * Return: if found or NULL returns path of command
+*/
+
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int x = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[x] || pathstr[x] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, x);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[x])
+				break;
+			curr_pos = x;
+		}
+		x++;
+	}
+	return (NULL);
+}
+
