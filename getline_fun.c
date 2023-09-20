d01d81483b49e853919283c50922f0927a3b444a
Esraa413 <esraaabdelwahab614@gmail.com>
2023-09-20 12:47:59 -0700

    shell

--- getline_fun.c
+++ getline_fun.c
@@ -63,7 +63,9 @@ ssize_t get_input(info_t *info)
 	_putchar(BUF_FLUSH);
 	r = input_buf(info, &buf, &len);
 	if (r == -1)
+	{
 		return (-1);
+	}
 	if (len)
 	{
 		j = i;
@@ -102,10 +104,16 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 	ssize_t r = 0;
 
 	if (*i)
+	{
 		return (0);
+	}
+
 	r = read(info->readfd, buf, READ_BUF_SIZE);
 	if (r >= 0)
+	{
 		*i = r;
+	}
+
 	return (r);
 }
 
@@ -128,17 +136,25 @@ int _getline(info_t *info, char **ptr, size_t *length)
 
 	p = *ptr;
 	if (p && length)
+	{
 		s = *length;
+	}
 	if (i == len)
+	{
 		i = len = 0;
+	}
 	r = read_buf(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
+	{
 		return (-1);
+	}
 	c = _strchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
 	if (!new_p)
+	{
 		return (p ? free(p), -1 : -1);
+	}
 	if (s)
 		_strncat(new_p, buf + i, k - i);
 	else
@@ -147,7 +163,9 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	i = k;
 	p = new_p;
 	if (length)
+	{
 		*length = s;
+	}
 	*ptr = p;
 	return (s);
 }
@@ -162,6 +180,8 @@ int _getline(info_t *info, char **ptr, size_t *length)
 void sigintHandler(__attribute__((unused))int sig_num)
 {
 	_puts("\n");
+
 	_puts("$ ");
+
 	_putchar(BUF_FLUSH);
 }
