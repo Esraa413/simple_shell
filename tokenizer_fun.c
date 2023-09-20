4ce2afb5e2bfb8b03d41944434fb7a6cd1b4893a
Esraa413 <esraaabdelwahab614@gmail.com>
2023-09-20 13:59:34 -0700

    shell

--- tokenizer_fun.c
+++ tokenizer_fun.c
@@ -17,35 +17,56 @@ char **strtow(char *str, char *d)
 	char **s;
 
 	if (str == NULL || str[0] == 0)
+	{
 		return (NULL);
+	}
 	if (!d)
+	{
 		d = " ";
-	for (i = 0; str[i] != '\0'; i++)
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
 		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+		{
 			numwords++;
+		}
+		i++;
+	}
 
 	if (numwords == 0)
+	{
 		return (NULL);
+	}
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
+	{
 		return (NULL);
+	}
 	for (i = 0, j = 0; j < numwords; j++)
 	{
 		while (is_delim(str[i], d))
 			i++;
-		k = 0;
-		while (!is_delim(str[i + k], d) && str[i + k])
-			k++;
+		for (k = 0; !is_delim(str[i + k], d) && str[i + k]; k++)
+			;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
 		{
-			for (k = 0; k < j; k++)
+			k = 0;
+			while (k < j)
+			{
 				free(s[k]);
+				k++;
+			}
 			free(s);
 			return (NULL);
 		}
-		for (m = 0; m < k; m++)
+		m = 0;
+		while (m < k)
+		{
 			s[j][m] = str[i++];
+			m++;
+		}
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
@@ -65,33 +86,53 @@ char **strtow2(char *str, char d)
 	char **s;
 
 	if (str == NULL || str[0] == 0)
+	{
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
 		if ((str[i] != d && str[i + 1] == d) ||
 		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+		{
 			numwords++;
+		}
+		i++;
+	}
 	if (numwords == 0)
+	{
 		return (NULL);
+	}
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
+	{
 		return (NULL);
-	for (i = 0, j = 0; j < numwords; j++)
+	}
+	for (i = 0; j = 0; j < numwords; j++)
 	{
 		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
-		while (str[i + k] != d && str[i + k] && str[i + k] != d)
-			k++;
+		for (k = 0; str[i + k] != d && str[i + k] && str[i + k] != d k++)
+			;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
 		{
-			for (k = 0; k < j; k++)
+			k = 0;
+			while (k < j)
+			{
 				free(s[k]);
+				k++;
+			}
 			free(s);
 			return (NULL);
 		}
-		for (m = 0; m < k; m++)
+		m = 0;
+		while (m < k)
+		{
 			s[j][m] = str[i++];
+			m++;
+		}
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
