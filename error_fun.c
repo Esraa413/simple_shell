ac307767f365b3e622ea326fd2b0cb6ef57b988a
Esraa413 <esraaabdelwahab614@gmail.com>
2023-09-20 10:01:15 -0700

    shell

--- error_fun.c
+++ error_fun.c
@@ -9,24 +9,27 @@
 
 int _erratoi(char *s)
 {
-	int i = 0;
-	unsigned long int result = 0;
+	int x = 0;
+	unsigned long int resu = 0;
 
 	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
+		s++;  /* TODO: why  make main return 255? */
+	while (s[x] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[x] >= '0' && s[x] <= '9')
 		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
+			resu *= 10;
+			resu += (s[x] - '0');
+			if (resu > INT_MAX)
 				return (-1);
 		}
 		else
+		{
 			return (-1);
+		}
+		x++;
 	}
-	return (result);
+	return (resu);
 }
 /**
  * print_error - error message
@@ -58,8 +61,8 @@ void print_error(info_t *info, char *estr)
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
+	int x, num = 0;
+	unsigned int _abs_, cur;
 
 	if (fd == STDERR_FILENO)
 		__putchar = _eputchar;
@@ -67,24 +70,24 @@ int print_d(int input, int fd)
 	{
 		_abs_ = -input;
 		__putchar('-');
-		count++;
+		num++;
 	}
 	else
 		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
+	cur = _abs_;
+	for (x = 1000000000; x > 1; x /= 10)
 	{
-		if (_abs_ / i)
+		if (_abs_ / x)
 		{
-			__putchar('0' + current / i);
-			count++;
+			__putchar('0' + cur / x);
+			num++;
 		}
-		current %= i;
+		cur %= x;
 	}
-	__putchar('0' + current);
-	count++;
+	__putchar('0' + cur);
+	num++;
 
-	return (count);
+	return (num);
 }
 
 /**
@@ -100,14 +103,14 @@ char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
 	static char buffer[50];
-	char sign = 0;
+	char sign_in = 0;
 	char *ptr;
 	unsigned long n = num;
 
 	if (!(flags & CONVERT_UNSIGNED) && num < 0)
 	{
 		n = -num;
-		sign = '-';
+		sign_in = '-';
 
 	}
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
@@ -119,26 +122,30 @@ char *convert_number(long int num, int base, int flags)
 		n /= base;
 	} while (n != 0);
 
-	if (sign)
-		*--ptr = sign;
+	if (sign_in)
+	{
+		*--ptr = sign_in;
+	}
 	return (ptr);
 }
 
 /**
  * remove_comments - first instance of '#' with '\0'
- * @buffer: address to string
+ * @buf: address to string
  *
  * Return: Always 0;
  */
 
 void remove_comments(char *buf)
 {
-	int i;
+	int x;
 
-	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+	x = 0;
+	while (buf[x] != '\0')
+		if (buf[x] == '#' && (!x || buf[x - 1] == ' '))
 		{
-			buf[i] = '\0';
+			buf[x] = '\0';
 			break;
 		}
+	x++;
 }
